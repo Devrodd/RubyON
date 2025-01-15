884b0c1db53bda92b27c4558b006a198057c4e98
rodrigodantc <rodrigodantasje@gmail.com>
2025-01-14 23:47:06 -0300

    Subi uma atualizacao

---
+++ condi.rb
@@ -0,0 +1,13 @@
+puts "Advinhe um sabor de sorvete"
+
+sorvete = gets.to_i
+
+if sorvete == "chocolate" 
+
+  puts "Parabéns! O sabor escolhido foi o chocolate."
+
+else 
+
+  puts "Tente novamente, o sabor era chocolate."
+
+end
